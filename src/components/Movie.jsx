310f9ad4f0fb3a55848c061df36b55205a4dd85a
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-04-19 21:40:09 +0100

    add MovieDetails

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,49 @@
-const Movie = () => {
+const Movie = (props) => {
   return (
-    <div>
-      <img src="" alt="" />
-    </div>
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto flex flex-wrap">
+        <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+          <img
+            alt="feature"
+            class="object-cover object-center h-full w-full"
+            src={props.img}
+          />
+        </div>
+        <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+          <div class="flex flex-col mb-10 lg:items-start items-center">
+            <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5"></div>
+            <div class="flex-grow">
+              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                {props.name}
+              </h2>
+              <p class="leading-relaxed text-base">{props.summary}</p>
+            </div>
+          </div>
+          <div class="flex flex-col mb-10 lg:items-start items-center">
+            <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5"></div>
+            <div class="flex-grow">
+              <h2 class="text-gray-900 text-lg title-font font-medium mb-3"></h2>
+              <p class="leading-relaxed text-base">
+                Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                taxidermy. Gastropub indxgo juice poutine.
+              </p>
+            </div>
+          </div>
+          <div class="flex flex-col mb-10 lg:items-start items-center">
+            <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5"></div>
+            <div class="flex-grow">
+              <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                Neptune
+              </h2>
+              <p class="leading-relaxed text-base">
+                Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+                taxidermy. Gastropub indxgo juice poutine.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,8 +1,30 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-
+import Movie from "../components/Movie";
 const MovieDetail = () => {
-  return <div></div>;
+  const [movie, setMovie] = useState({
+    image: {},
+    genres: [],
+  });
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    fetchMovie();
+  }, []);
+
+  const fetchMovie = () => {
+    fetch(`https://api.tvmaze.com/shows/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovie(data);
+      });
+  };
+  return (
+    <div>
+      <Movie img={movie.image.original} summary={movie.summary} />
+    </div>
+  );
 };
 
 export default MovieDetail;
